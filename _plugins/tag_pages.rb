446fec56ca7e6d5f1442109bb31d558549191fd6
Martin Sivak <msivak@redhat.com>
2016-05-18 23:53:21 +0200

    Stranky pro kategorie a stitky

--- _plugins/tag_pages.rb
+++ _plugins/tag_pages.rb
@@ -11,7 +11,7 @@ module Jekyll
       self.read_yaml(File.join(base, '_layouts'), 'tag.html')
       self.data['tag'] = tag
 
-      tag_title_prefix = site.config['tag_title_prefix'] || 'Nálepka: '
+      tag_title_prefix = site.config['tag_title_prefix'] || 'Šťítek: '
       self.data['title'] = "#{tag_title_prefix}#{tag}"
     end
   end
@@ -21,9 +21,38 @@ module Jekyll
 
     def generate(site)
       if site.layouts.key? 'tag'
-        dir = site.config['tag_dir'] || 'tag'
+        dir = site.config['tag_dir'] || 'stitek'
         site.tags.each_key do |tag|
           site.pages << TagPage.new(site, site.source, File.join(dir, tag), tag)
+        end
+      end
+    end
+  end
+
+  class CatPage < Page
+    def initialize(site, base, dir, tag)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = 'index.html'
+
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'category.html')
+      self.data['category'] = tag
+
+      tag_title_prefix = site.config['tag_title_prefix'] || 'Kategorie: '
+      self.data['title'] = "#{tag_title_prefix}#{tag}"
+    end
+  end
+
+  class CatPageGenerator < Generator
+    safe true
+
+    def generate(site)
+      if site.layouts.key? 'category'
+        dir = site.config['category_dir'] || 'kategorie'
+        site.categories.each_key do |tag|
+          site.pages << CatPage.new(site, site.source, File.join(dir, tag), tag)
         end
       end
     end
